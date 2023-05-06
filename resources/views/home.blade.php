8819eefd7552ebd8c8b4cbf69febd64d2c5770b1
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-06 17:50:34 +0800

    refactor class for image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,9 +22,9 @@
 </div> -->
 
 <div class="container">
-    <div class="row">
+    <div class="row align-items-center">
         <div class="col-3">
-            <img src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" class="rounded-circle" alt="a dog" style="height: 10em" />
+            <img class="rounded-circle" src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"  alt="a dog" style="height: 10em" />
         </div>
         <div class="col-9">
             <div>
