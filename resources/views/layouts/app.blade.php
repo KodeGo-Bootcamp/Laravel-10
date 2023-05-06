63c8436fe3c59232507e300d0196186215379e8e
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-06 16:34:00 +0800

    add laravel telescope

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,11 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- <title>{{ config('app.name', 'Laravel') }}</title> -->
+
+    <title><?php echo e(config('app.name', 'Laravel')) ?></title>
+
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
