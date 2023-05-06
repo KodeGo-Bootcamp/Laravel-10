336be4a1f81de8a117cbe81bc4fdaafb2231bb03
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-06 17:35:12 +0800

    add design image in dashboard in laravel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container bg-dark">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -16,6 +16,16 @@
 
                     {{ __('You are logged in!') }}
                 </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-3">
+            <div class="col-9">
+                <img src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*" alt="a dog" />
             </div>
         </div>
     </div>
