a1dbc663ee49e97e34f6e37b3524640142d713a4
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-06 18:19:52 +0800

    repair telecope

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,7 +23,7 @@
 
 <div class="container">
     <div class="row align-items-center">
-        <div class="col-3">
+        <div class="col-2 p-2">
             <img class="rounded-circle" src="https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"  alt="a dog" style="height: 10em" />
         </div>
         <div class="col-9">
@@ -35,6 +35,9 @@
                 <div><strong>20k</strong> Followers</div>
                 <div><strong>100k</strong> Following</div>
             </div>
+            <div class="pt-2"><p>@test_user<p></div>
+            <div class=""><p>I love laravel and I just want to explore more of it.</p></div>
+            <div class=""><a target="_blank" href="https://renstrio24p.github.io/Mini-Project-Porfolio/" class="">Renstrio Page</a></div>
         </div>
     </div>
 </div>
