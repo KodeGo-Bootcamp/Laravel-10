09d1b3588f1355177630f6ec28b72a80bcef069d
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-15 20:08:45 +0800

    create symbolic links

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,22 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class PostsController extends Controller
 {
-    public function create(){
+    // Authentication for Protecting some routes inside of that post controllers
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function create()
+    {
         return view('posts.create');
     }
-    public function store(){
+
+    public function store()
+    {
         $data = request()->validate([
-            'caption'=>'required',
-            'image'=>['required','image']
+            'caption' => 'required',
+            'image' => ['required', 'image'],
         ]);
 
         // $post = new \App\Models\Post();
         // $post->caption = $data['caption'];
         // $post->save();
+
+        dd(request('image')->store('uploads','public'));
 
         auth()->user()->posts()->create($data);
 
@@ -25,7 +35,4 @@ class PostsController extends Controller
 
         dd(request()->all());
     }
-
-
-    
 }
