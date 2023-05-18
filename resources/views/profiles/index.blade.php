d71f0a14731377a237228c99db17f36d81591e86
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-18 19:46:30 +0800

    setup connection to p/create post

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -29,7 +29,7 @@
         <div class="col-9">
             <div class="d-flex align-items-center">
                 <!-- <h1>Snippy Blog</h1> -->
-                <a href="#0" class="btn btn-primary p-1" style="margin: 0 20px 0 0"> New Post </a>
+                <a href="/p/create" class="btn btn-primary p-1" style="margin: 0 20px 0 0"> New Post </a>
                 <h1>{{$user->username}}</h1>
             </div>
             <div class="d-flex align-items-center gap-3">
@@ -40,15 +40,15 @@
             <div class="pt-2"><p>{{$user->profile->title}}<p></div>
             <div class=""><p>{{$user->profile->description}}</p></div>
             <div class=""><a target="_blank" href="https://renstrio24p.github.io/Mini-Project-Porfolio/" class="">{{$user->profile->url}}</a></div>
-            <div class="row">
-                @foreach($user->posts as $post)
-                <div class="col-4 p-3"><img class="w-100" src="/storage/{{$post->image}}" alt=""></div>
-                @endforeach
-                <!--  Add 3 divs and nest them together with a single div (8:31pm)  -->
-                <!-- <div class="col-4 p-3"><img class="w-100" src="https://source.unsplash.com/1600x900/?cat" alt=""></div>
-                <div class="col-4 p-3"><img class="w-100" src="https://source.unsplash.com/1600x900/?cat" alt=""></div>
-                <div class="col-4 p-3"><img class="w-100" src="https://source.unsplash.com/1600x900/?cat" alt=""></div> -->
-            </div>
+        </div>
+        <div class="row">
+            @foreach($user->posts as $post)
+            <div class="col-4 p-3"><img class="w-100" src="/storage/{{$post->image}}" alt=""></div>
+            @endforeach
+            <!--  Add 3 divs and nest them together with a single div (8:31pm)  -->
+            <!-- <div class="col-4 p-3"><img class="w-100" src="https://source.unsplash.com/1600x900/?cat" alt=""></div>
+            <div class="col-4 p-3"><img class="w-100" src="https://source.unsplash.com/1600x900/?cat" alt=""></div>
+            <div class="col-4 p-3"><img class="w-100" src="https://source.unsplash.com/1600x900/?cat" alt=""></div> -->
         </div>
     </div>
 </div>
